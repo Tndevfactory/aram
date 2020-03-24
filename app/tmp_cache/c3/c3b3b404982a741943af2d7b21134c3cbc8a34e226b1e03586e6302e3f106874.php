4f6e33e88789a3fda35540708f45468586efa454
Tndevfactory <tndevfactory@gmail.com>
2020-03-24 17:17:37 +0100

    page ride

--- app/tmp_cache/c3/c3b3b404982a741943af2d7b21134c3cbc8a34e226b1e03586e6302e3f106874.php
+++ app/tmp_cache/c3/c3b3b404982a741943af2d7b21134c3cbc8a34e226b1e03586e6302e3f106874.php
@@ -24,54 +24,23 @@ class __TwigTemplate_537ce187e3d2df259903b7378481e7a13dfd126e85980720fadf2c51316
 
         $this->source = $this->getSourceContext();
 
-        $this->parent = false;
-
         $this->blocks = [
             'title' => [$this, 'block_title'],
+            'content' => [$this, 'block_content'],
         ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "layout.twig";
     }
 
     protected function doDisplay(array $context, array $blocks = [])
     {
         $macros = $this->macros;
-        // line 1
-        echo " 
- 
-";
-        // line 3
-        $this->displayBlock('title', $context, $blocks);
-        // line 4
-        echo " <!-- besoins/fr/ride-->
-  <section id=\"about\" class=\"py-3\">
-    <div class=\"container\">
-      <div class=\"row\">
-        <div class=\"col-md-6\">
-                
-          <h1>";
-        // line 10
-        echo twig_escape_filter($this->env, ($context["LANG"] ?? null), "html", null, true);
-        echo "</h1>
-
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
-            error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
-            laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
-            praesentium temporibus adipisci debitis labore!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
-            error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
-            laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
-            praesentium temporibus adipisci debitis labore!</p>
-        </div>
-        <div class=\"col-md-6\">
-          <img src=\"";
-        // line 22
-        echo twig_escape_filter($this->env, ($context["URLROOT"] ?? null), "html", null, true);
-        echo "/public/img/quisommenous/quisommenous2.jpg\" alt=\"\" class=\"img-fluid rounded-circle d-none d-md-block about-img\">
-        </div>
-      </div>
-    </div>
-  </section>
-
-  ";
+        $this->parent = $this->loadTemplate("layout.twig", "\\homes\\besoins\\fr\\visage\\rides.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
     }
 
     // line 3
@@ -79,6 +48,58 @@ class __TwigTemplate_537ce187e3d2df259903b7378481e7a13dfd126e85980720fadf2c51316
     {
         $macros = $this->macros;
         echo "  <title> besoins/fr/ride </title> ";
+    }
+
+    // line 5
+    public function block_content($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 6
+        echo " 
+
+    <!-- besoins/fr/ride-->
+      <section id=\"ride\" class=\"py-3\">
+        <div class=\"container\">
+          <div class=\"row\">
+          
+            <div class=\"col-md-6\">
+              <h1>";
+        // line 14
+        echo twig_escape_filter($this->env, ($context["LANG"] ?? null), "html", null, true);
+        echo "</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                praesentium temporibus adipisci debitis labore!</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                praesentium temporibus adipisci debitis labore!</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                praesentium temporibus adipisci debitis labore!</p>  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                praesentium temporibus adipisci debitis labore!</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                praesentium temporibus adipisci debitis labore!</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                praesentium temporibus adipisci debitis labore!</p>
+            
+            </div>
+
+            <div class=\"col-md-6\">
+              <img src=\"";
+        // line 38
+        echo twig_escape_filter($this->env, ($context["URLROOT"] ?? null), "html", null, true);
+        echo "/public/img/quisommenous/quisommenous2.jpg\" alt=\"\" class=\"img-fluid rounded-circle d-none d-md-block about-img\">
+            </div>
+
+          </div>
+        </div>
+      </section>
+";
     }
 
     public function getTemplateName()
@@ -93,38 +114,55 @@ class __TwigTemplate_537ce187e3d2df259903b7378481e7a13dfd126e85980720fadf2c51316
 
     public function getDebugInfo()
     {
-        return array (  78 => 3,  67 => 22,  52 => 10,  44 => 4,  42 => 3,  38 => 1,);
+        return array (  95 => 38,  68 => 14,  58 => 6,  54 => 5,  47 => 3,  36 => 1,);
     }
 
     public function getSourceContext()
     {
-        return new Source(" 
- 
+        return new Source(" {% extends 'layout.twig' %}
+
 {% block title %}  <title> besoins/fr/ride </title> {% endblock  %}
- <!-- besoins/fr/ride-->
-  <section id=\"about\" class=\"py-3\">
-    <div class=\"container\">
-      <div class=\"row\">
-        <div class=\"col-md-6\">
-                
-          <h1>{{LANG}}</h1>
+  
+{% block content %}
+ 
 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
-            error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
-            laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
-            praesentium temporibus adipisci debitis labore!</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
-            error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
-            laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
-            praesentium temporibus adipisci debitis labore!</p>
-        </div>
-        <div class=\"col-md-6\">
-          <img src=\"{{URLROOT}}/public/img/quisommenous/quisommenous2.jpg\" alt=\"\" class=\"img-fluid rounded-circle d-none d-md-block about-img\">
-        </div>
-      </div>
-    </div>
-  </section>
+    <!-- besoins/fr/ride-->
+      <section id=\"ride\" class=\"py-3\">
+        <div class=\"container\">
+          <div class=\"row\">
+          
+            <div class=\"col-md-6\">
+              <h1>{{LANG}}</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                praesentium temporibus adipisci debitis labore!</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                praesentium temporibus adipisci debitis labore!</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                praesentium temporibus adipisci debitis labore!</p>  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                praesentium temporibus adipisci debitis labore!</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                praesentium temporibus adipisci debitis labore!</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                praesentium temporibus adipisci debitis labore!</p>
+            
+            </div>
 
+            <div class=\"col-md-6\">
+              <img src=\"{{URLROOT}}/public/img/quisommenous/quisommenous2.jpg\" alt=\"\" class=\"img-fluid rounded-circle d-none d-md-block about-img\">
+            </div>
+
+          </div>
+        </div>
+      </section>
+{% endblock  %}
   ", "\\homes\\besoins\\fr\\visage\\rides.twig", "C:\\xampp\\htdocs\\ARAM\\app\\views\\homes\\besoins\\fr\\visage\\rides.twig");
     }
 }
